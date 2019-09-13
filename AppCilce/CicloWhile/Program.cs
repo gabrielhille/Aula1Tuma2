ff2acf1047b9497b5d3a1717209e1c8ea374455f
gabrielhille1990@gmail.com <gabrielhille1990@gmail.com>
2019-09-13 13:46:10 -0300

    nomeIdade

--- AppCilce/CicloWhile/Program.cs
+++ AppCilce/CicloWhile/Program.cs
@@ -12,12 +12,23 @@ namespace CicloWhile
         {
             ShowInitAppText();
 
+            string nome = string.Empty;
+            int idade = 0;
+
+
            
 
             while(AskToContinue () == 1)
             {
-                VocePodeBeber();
+               nome = AskToName();
+               idade = VocePodeBeber(nome );
+
+
             }
+            Console.WriteLine($"\r\nSuas informações: Nome {nome} e sua idade{idade}");
+            Console.ReadKey();
+
+
         }
         /// <summary>
         /// Metodo para mostrar o texto inicial do sistema alone
@@ -43,24 +54,29 @@ namespace CicloWhile
         /// <summary>
         /// pergunda a idade do usuario
         /// retorne se ele pode beber </summary>
-        private static void VocePodeBeber()
+
+        private static int VocePodeBeber(string nome)
         {
-            Console.WriteLine("Qual a sua idade?");
+
+            Console.WriteLine("\r\nQual a sua idade?");
             int idade = int.Parse(Console.ReadLine());
 
             if( idade < 18)
-             Console.WriteLine("você não pode beber");
+             Console.WriteLine("\r\nVocê não pode beber");
             else
-                Console.WriteLine("você pode beber");
+                Console.WriteLine($"\r\nBora{nome}, fdbora toma uma");
+            return idade;
 
+        }
+        /// <summary>
+        /// metodo para perguntar o nome
+        /// </summary>
+        /// <returns>retorna o nome informado</returns>
+        private static string AskToName()
+        {
 
-
-
-
-
-
-
-
+            Console.WriteLine("\r\n Qual seu nome?");
+            return Console.ReadLine();
 
         }
          
