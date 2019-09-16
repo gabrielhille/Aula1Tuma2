d82bd80e64ba1094655420f9693227a56a44b964
gabrielhille1990@gmail.com <gabrielhille1990@gmail.com>
2019-09-16 17:13:30 -0300

    Aula_16/09/19_parte2

---
+++ LacosDeRepeticaoParte2/AprendendoLisy/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprendendoList
+{
+    class Program
+    {
+        static List<string> minhaListaPulgmatica = new List<string>()
+            {
+                "Felipe",
+                "Irineu",
+                "Serilop"
+            };
+        static void Main(string[] args)
+        {
+            AdicionarItensALista();
+            minhaListaPulgmatica.Add(Console.ReadLine());
+            ListaInformacoes();
+            
+            Console.ReadKey();
+
+
+        }
+        private static void AdicionarItensALista()
+        {
+            Console.Clear();
+            Console.WriteLine("Informe um nome");
+            minhaListaPulgmatica.Add(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Deseja informar mais um nome (s) sim qualquer outra não");
+
+            if (Console.ReadKey().KeyChar.ToString().ToLower() == "s")
+                AdicionarItensALista();
+        }
+
+        private static void ListaInformacoes()
+        {
+            Console.Clear();
+
+            foreach (var item in minhaListaPulgmatica)
+                Console.WriteLine(item.ToUpper());
+               Console.Write("Adicionado com sucesso");
+
+
+        }
+        
+
+    }
+}
---
+++ LacosDeRepeticaoParte2/ApresentarApenasOsDeMaior/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApresentarApenasOsDeMaior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conteudo = "nome:Felipe,idade:27;nome:Giomar,idade:17;nome:Edson,Idade:19;nome:Ericledson,idade:75;nome:Junior,idade:45";
+
+            var listaDeInformacoes = conteudo.Split(';');
+
+            Console.WriteLine("Usuarios cadastrados no sistema:");
+
+            foreach (var item in listaDeInformacoes)
+            {
+                Console.WriteLine(item.Split(',')[0]);
+
+            }
+            Console.Write("Insira o nome: ");
+            var nomeBusca = Console.ReadLine();
+
+            foreach (var item in listaDeInformacoes)
+            {
+                var informacoesSplit = item.Split(',');
+
+                var nome = informacoesSplit[0].Split(':')[1];
+                var idade = informacoesSplit[1].Split(':')[1];
+
+                if (int.Parse(idade) >= 18 && nomeBusca == nome)
+                    Console.WriteLine($"o {nome} é maior de 18 anos");
+                if (int.Parse(idade) <= 18 && nomeBusca == nome)
+                    Console.WriteLine($"{nome} é menor de 18 anos.");
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
