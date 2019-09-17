1824fb4bc7664fc523b2845693dda82eb09a61b9
gabrielhille1990@gmail.com <gabrielhille1990@gmail.com>
2019-09-17 17:10:18 -0300

    desenho

---
+++ aulaDia_17-09-19/desenho/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desenho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("          0");
+            Console.WriteLine("         OOO");
+            Console.WriteLine("       OOOOOOO");
+            Console.WriteLine("      OOOOOOOOO");
+            Console.WriteLine("     OOOOOOOOOOO");
+            Console.WriteLine("    OOOOOOOOOOOOO");
+            Console.WriteLine("   OOOOOOOOOOOOOOO");
+            Console.WriteLine("  OOOOOOOOOOOOOOOOO");
+            Console.WriteLine(" OOOOOOOOOOOOOOOOOOO");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          || ");
+            Console.WriteLine(" --------------------");
+            Console.WriteLine(" --------------------");
+
+
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.WriteLine("          0");
+            Console.WriteLine("         OOO");
+            Console.WriteLine("       OOOOOOO");
+            Console.WriteLine("      OOOOOOOOO");
+            Console.WriteLine("     OOOOOOOOOOO");
+            Console.WriteLine("    OOOOOOOOOOOOO");
+            Console.WriteLine("   OOOOOOOOOOOOOOO");
+            Console.WriteLine("  OOOOOOOOOOOOOOOOO");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          || ");
+            Console.WriteLine(" --------------------");
+            Console.WriteLine(" --------------------");
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.WriteLine("          0");
+            Console.WriteLine("         OOO");
+            Console.WriteLine("       OOOOOOO");
+            Console.WriteLine("      OOOOOOOOO");
+            Console.WriteLine("     OOOOOOOOOOO");
+            Console.WriteLine("    OOOOOOOOOOOOO");
+            Console.WriteLine("   OOOOOOOOOOOOOOO");
+            Console.WriteLine("          || ");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          || ");
+            Console.WriteLine(" --------------------");
+            Console.WriteLine(" --------------------");
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.WriteLine("          0");
+            Console.WriteLine("         OOO");
+            Console.WriteLine("       OOOOOOO");
+            Console.WriteLine("      OOOOOOOOO");
+            Console.WriteLine("     OOOOOOOOOOO");
+            Console.WriteLine("    OOOOOOOOOOOOO");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          || ");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          || ");
+            Console.WriteLine(" --------------------");
+            Console.WriteLine(" --------------------");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("          0");
+            Console.WriteLine("         OOO");
+            Console.WriteLine("       OOOOOOO");
+            Console.WriteLine("      OOOOOOOOO");
+            Console.WriteLine("     OOOOOOOOOOO");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          || ");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          || ");
+            Console.WriteLine(" --------------------");
+            Console.WriteLine(" --------------------");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("          0");
+            Console.WriteLine("         OOO");
+            Console.WriteLine("       OOOOOOO");
+            Console.WriteLine("      OOOOOOOOO");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          || ");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          || ");
+            Console.WriteLine(" --------------------");
+            Console.WriteLine(" --------------------");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("          0");
+            Console.WriteLine("         OOO");
+            Console.WriteLine("       OOOOOOO");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          || ");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          || ");
+            Console.WriteLine(" --------------------");
+            Console.WriteLine(" --------------------");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("           ");
+            Console.WriteLine("           ");
+            Console.WriteLine("           ");
+            Console.WriteLine("          || ");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          ||");
+            Console.WriteLine("          || ");
+            Console.WriteLine(" --------------------");
+            Console.WriteLine(" --------------------");
+            Console.ReadKey();
+
+
+
+        }
+
+
+    }
+}
