5135d99e64a99714b4b616058494ae4998790887
gabrielhille1990@gmail.com <gabrielhille1990@gmail.com>
2019-09-18 17:32:07 -0300

    ListaDeFrutas

---
+++ MinhaPrimeiraClass/MinhaPrimeiraClass/Model/Bicicleta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaPrimeiraClass.Model
+{
+    class Bicicleta
+    {
+        public string Marca { get; set; } 
+
+        public int pneus { get; } = 2;
+
+    }
+}
