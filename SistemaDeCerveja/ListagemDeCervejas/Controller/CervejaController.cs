fc48f97264f2de9f4444048e3092ed7bce27d86b
gabrielhille1990@gmail.com <gabrielhille1990@gmail.com>
2019-09-30 16:24:00 -0300

    inicioSQL

---
+++ SistemaDeCerveja/ListagemDeCervejas/Controller/CervejaController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListagemDeCervejas.Model;
+
+namespace ListagemDeCervejas.Controller
+{
+    public class CervejaController
+    {
+        SistemaDeCervejaContext sistemaDeCervejaContext = new SistemaDeCervejaContext();
+        /// <summary>
+        /// metodo que retorna lista de cervejas
+        /// </summary>
+        /// <returns></returns>
+        public List<Cerveja> GetCervejas()
+        {
+            return sistemaDeCervejaContext.listaDeCerveja;
+        }
+
+
+        public double RetornarValor()
+        {
+           return  sistemaDeCervejaContext.listaDeCerveja.Sum(x => x.Valor);
+             
+        }
+        public double RetornarValorEmLitros()
+        {
+            return sistemaDeCervejaContext.listaDeCerveja.Sum(x => x.Litros);
+             
+        }
+
+        public void AdicionarCerveja(Cerveja cerveja)
+        {
+            var novoId = sistemaDeCervejaContext.listaDeCerveja.Count;
+            cerveja.Id = novoId++;
+
+            sistemaDeCervejaContext.listaDeCerveja.Add(cerveja);
+
+
+        }
+
+            
+
+    }
+
+}
---
+++ SistemaDeCerveja/ListagemDeCervejas/Model/SistemaDeCervejaContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListagemDeCervejas.Model
+{
+   
+        
+
+        public class SistemaDeCervejaContext
+        {
+
+            public List<Cerveja> listaDeCerveja { get; set; }
+
+            public SistemaDeCervejaContext()
+            {
+                listaDeCerveja = new List<Cerveja>();
+
+
+
+                listaDeCerveja.Add(new Cerveja()
+
+                {
+                    Id = 1,
+                    Nome = "Colorado",
+                    Litros = 0.6,
+                    Alcool = 8.5,
+                    Valor = 14.5,
+                });
+                listaDeCerveja.Add(new Cerveja()
+                {
+                    Id = 2,
+                    Nome = "Corona",
+                    Litros = 0.355,
+                    Alcool = 4.5,
+                    Valor = 5.5,
+                });
+                listaDeCerveja.Add(new Cerveja()
+                {
+                    Id = 3,
+                    Nome = "Skol",
+                    Litros = 0.350,
+                    Alcool = 4.5,
+                    Valor = 1.99,
+                });
+                listaDeCerveja.Add(new Cerveja()
+                {
+                    Id = 4,
+                    Nome = "Patagonia",
+                    Litros = 0.455,
+                    Alcool = 4.8,
+                    Valor = 5.6,
+                });
+                listaDeCerveja.Add(new Cerveja()
+                {
+                    Id = 5,
+                    Nome = "Antartica",
+                    Litros = 0.6,
+                    Alcool = 4.7,
+                    Valor = 6.25,
+                });
+
+
+
+
+
+            }
+        }  
+     
+}
