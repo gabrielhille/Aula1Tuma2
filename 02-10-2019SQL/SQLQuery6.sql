c0e75369ece98f11d31fbf7b1e70c8b198da5976
gabrielhille1990@gmail.com <gabrielhille1990@gmail.com>
2019-10-03 17:15:47 -0300

    sql03-10-19

---
+++ 02-10-2019SQL/SQLQuery6.sql
@@ -0,0 +1,11 @@
+﻿select * from Clientes;
+select * from Pedidos;
+
+Select cli. * from Clientes cli
+inner join Pedidos ped on cli.Id = ped.ClienteId
+where ped.Numero in ('N000000002','N000000004','N000000013')
+
+select * from Pedidos ped
+where ped.ClienteId in (select cli.Id from Clientes cli inner join
+Pedidos ped on cli.Id = ped.ClienteId where ped.Numero in ('N000000002','N000000004','N000000013'))
+order by ped.valor desc
