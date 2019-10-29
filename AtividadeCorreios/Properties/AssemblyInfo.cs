0bb7999a08aebeb946c40192a1ebb42eb8eb89ed
gabrielhille1990@gmail.com <gabrielhille1990@gmail.com>
2019-10-29 14:33:58 -0300

    commit correios

---
+++ AtividadeCorreios/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AtividadeCorreios")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AtividadeCorreios")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cb855c8a-f47f-420e-a543-3825f0c5849b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AtividadeCorreios/Scripts/Utils/Script.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    $('input[id=Submit1]').click(function () {
+
+        var codigo = $('input[name="codigo"]').val();
+        var senha = ``;
+        var servicos = $('select[name="servicos"]').val();
+        var cepori = $('input[name="cepori"]').val();
+        var cepdes = $('input[name="cepdes"]').val();
+        var peso = $('input[name="peso"]').val();
+        var comprimento = $('input[name="comprimento"]').val();
+        var formato = '1';
+        var altura = $('input[name="altura"]').val();
+        var largura = $('input[name="largura"]').val();
+        var diametro = $('input[name="diametro"]').val();
+        var maopropria = 'S';
+        var valordeclarado = $('input[name="valordeclarado"]').val();
+        var avisorecebimento = 'S';
+
+        var urlMontada = "http://usysweb.com.br/api/correiosambev.php?" + "&nCdEmpresa=" + codigo + "&sDsSenha=" + senha + "&nCdServico=" + servicos +
+            "&sCepOrigem=" + cepori + "&sCepDestino=" + cepdes + "&nVlPeso=" + peso + "&nCdFormato=" + formato + "&nVlComprimento=" + comprimento + "&nVlAltura=" + altura +
+            "&nVlLargura=" + largura + "&nVlDiametro=" + diametro + "&sCdMaoPropria=" + maopropria + "&nVlValorDeclarado=" + valordeclarado + "&sCdAvisoRecebimento=" + avisorecebimento;
+
+         
+        $.getJSON(urlMontada, function (tipo) {
+            
+            alert("O preço desse pedido será: R$ " + tipo.cServico.Valor +
+                ", prazo de entrga é de " + tipo.cServico.PrazoEntrega + "Dias"
+
+            );
+            
+        });       
+    });           
+});                         
+                  
+                  
